bf69c5c22981b5d54b261564eeb8b83d996fd8b4
Tony071101 <tinpigboyapple07112001@gmail.com>
2024-06-18 15:04:19 +0700

    add: Level Manager object and script.

--- Assets/Scripts/GameSystem/EnemySpawner.cs
+++ Assets/Scripts/GameSystem/EnemySpawner.cs
@@ -6,24 +6,31 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPref;
+    [SerializeField] private LevelManager levelManager;
     private int initialEnemyCount = 5;
     private float spawnRangeX = 190f;
     private float spawnRangeZ = 190f;
     private int currentWave = 0;
     private List<GameObject> enemies = new List<GameObject>();
 
-    private void Start() {
-        StartCoroutine(SpawnWaves());
+    public void StartLevel(int level)
+    {
+        StopAllCoroutines();
+        currentWave = 0;
+        initialEnemyCount = 5 + (level - 1) * 2;
+        StartCoroutine(SpawnWaves(level));
     }
 
-    private IEnumerator SpawnWaves()
+    private IEnumerator SpawnWaves(int level)
     {
+        int maxWaves = 3 + (level / 2);
         while (currentWave < 3)
         {
             yield return new WaitForSeconds(5f);
             currentWave++;
-            int enemyCount = initialEnemyCount + (currentWave - 1) * 5;
+            int enemyCount = initialEnemyCount + (currentWave - 1) * 2; //Plus + 2 enemies each waves.
 
+            Debug.Log("Wave: " + currentWave);
             for (int i = 0; i < enemyCount; i++)
             {
                 SpawnEnemy();
@@ -33,6 +40,8 @@ public class EnemySpawner : MonoBehaviour
             yield return new WaitUntil(() => enemies.Count == 0);
             // Tăng số lượng kẻ thù cho wave tiếp theo
         }
+
+        levelManager.OnAllEnemiesDefeated();
     }
 
     private void SpawnEnemy()
